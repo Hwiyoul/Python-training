fcf7c8b7626049c78288a9f98a93b1c9f3d19430
Hwiyoul <blackpolarbear7@gmail.com>
2015-05-20 18:59:10 -0400

    Update finding_gcd.py

--- finding_gcd.py
+++ finding_gcd.py
@@ -1,5 +1,5 @@
 def finding_gcd(a,b):
-    '''implements the greatest common divider algorithm'''
+    'implements the greatest common divider algorithm'
     result, i = 0, 1
     while (i <= a):
         if (a % i == 0) & (b % i == 0):
